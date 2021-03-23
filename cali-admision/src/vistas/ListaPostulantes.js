e91067bdb99e8b7853867a108f2eaeec5bf90697
DiegoCastro4545 <090209@unsaac.edu.pe>
2021-03-23 13:38:43 -0500

    cambio diego

---
+++ cali-admision/src/vistas/ListaPostulantes.js
@@ -0,0 +1,14 @@
+import { Container } from '@material-ui/core';
+import React, { Component } from 'react';
+
+class ListaPostulantes extends Component {
+    render() {
+        return (
+            <Container>
+                
+            </Container>
+        );
+    }
+}
+
+export default ListaPostulantes;
